c67431c3a63f8b92439b7d7fa8cfd185410678a9
salvadorgonzalez96 <99943581+salvadorgonzalez96@users.noreply.github.com>
2022-02-19 03:28:00 -0600

    Update ActivityInsert.java

--- app/src/main/java/com/example/pm1_e1_0090_0300/ActivityInsert.java
+++ app/src/main/java/com/example/pm1_e1_0090_0300/ActivityInsert.java
@@ -5,13 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+
+import java.io.File;
 
 import com.example.pm1_e1_0090_0300.transactions.Transactions;
 
@@ -20,10 +30,24 @@ public class ActivityInsert extends AppCompatActivity {
     Spinner spaises;
     String pais;
 
+    Button btnCamara;
+    ImageView imgView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
+
+        btnCamara = findViewById(R.id.btnCamara);
+        imgView = findViewById(R.id.foto);
+
+        btnCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirCamara();
+            }
+        });
 
         spaises = findViewById(R.id.vePaises);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.array_countries, android.R.layout.simple_spinner_item);
@@ -93,4 +117,23 @@ public class ActivityInsert extends AppCompatActivity {
         nota.setText("");
         spaises.setSelection(0);
     }
+
+
+    private void abrirCamara(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            imgView.setImageBitmap(imgBitmap);
+        }
+    }
+
+
 }
